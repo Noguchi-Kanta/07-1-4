c0346bf91a84525f40f1420228fbe569fbbb96be
Noguchi-Kanta <a21ja115@st.osaka-cu.ac.jp>
2023-08-07 15:55:39 +0900

    詳細->一覧

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,8 +8,12 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index(Post $post)
- {
-    return view('posts/index')->with(['posts' => $post->getPaginateByLimit(5)]);
- 
- }
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
+    }
+   
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+    }
 }
